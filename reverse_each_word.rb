0bf039f5a63b1a0682cf35e04ebbc1483e8461ca
Judy McGoldrick <jjeanius@verizon.net>
2017-12-10 05:31:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word(sentence)
-  sentence.reverse_each_word do |sentence1|
-    sentence1
-end
-end
-
-def reverse_each_word(sentence)
-  sentence.collect do |sentence2.reverse|
-    sentence2
-end
+  sentence.collect do |sentence2|
+    sentence2.revere
+  end
