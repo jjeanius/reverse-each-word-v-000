589e3ed2dd5edec20f48b200ea250d0d29559a3b
Judy McGoldrick <jjeanius@verizon.net>
2017-12-10 04:36:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 def reverse_each_word(sentence)
   sentence = "Hello there, and how are you?".reverse.split(" ")
-  sentence.map reverse_each_word do |sentence1|
+  sentence.each reverse_each_word do |sentence1|
     sentence1
   end
 end
 
 
 def reverse_each_word(sentence)
-  sentence = "Hello there, and how are you?".revers_word.split(" ")
+  sentence = "Hello there, and how are you?".revers.split(" ")
   sentence.collect do |sentence2|
     sentence2
   end
